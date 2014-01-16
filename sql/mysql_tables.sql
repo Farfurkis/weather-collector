a4d60818f35a059faecf949cf67bb87f33cc153e
Pasha <pkundirenko>
2014-01-16 21:59:28 +0200

    MySQL database creation script added.

---
+++ sql/mysql_tables.sql
@@ -0,0 +1,25 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS `measurers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT 'Weather measurer name',
+  `description` text COMMENT 'Weather measurer description',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+CREATE TABLE IF NOT EXISTS `weather` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `measurer_id` int(10) unsigned NOT NULL,
+  `temperature` decimal(4,2) NOT NULL COMMENT 'Temperature in celcius',
+  `humidity` decimal(5,2) NOT NULL COMMENT 'Humidity in percents',
+  `measure_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Measure date and time',
+  PRIMARY KEY (`id`),
+  KEY `measure_date` (`measure_date`),
+  KEY `measurer_id` (`measurer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=845 ;
+
+
+ALTER TABLE `weather`
+  ADD CONSTRAINT `weather_ibfk_1` FOREIGN KEY (`measurer_id`) REFERENCES `measurers` (`id`) ON UPDATE CASCADE;
